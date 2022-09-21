9571d53d5604f197cc51af0f884e554f7fbc77ad
Roman <85025748+rmn-ykimov@users.noreply.github.com>
2022-09-22 00:59:42 +0400

    switching function update

--- switcher.py
+++ switcher.py
@@ -7,7 +7,10 @@ import wifi_credentials
 
 
 class NetworkInterface:
-    description = "Describes network device"
+    """
+    Network interface class
+    """
+    description = "Describes network interface"
 
     def __init__(self, name, isactive, traffic_transmission, username, password):
         self.name = name
@@ -31,7 +34,6 @@ class NetworkInterface:
         return os.system("ping -c 60 " + ip_values.HOST)
 
 
-# Objects of the class (wi-fi and lte) and its initial state
 wifi = NetworkInterface("wlan1", True, True, wifi_credentials.USERNAME, wifi_credentials.PASSWORD)
 """
 Wi-Fi interface's initial state
@@ -49,7 +51,7 @@ def switching():
     if wifi.host_response() == 0:
         wifi.traffic_transmission = True
     elif wifi.host_response() != 0:
-        if lte.google_response() == 0 and wifi.host_response() != 0:
+        while lte.google_response() == 0 and wifi.host_response() != 0:
             lte.isactive = True
             lte.traffic_transmission = True
             wifi.traffic_transmission = False
@@ -57,3 +59,5 @@ def switching():
             lte.traffic_transmission = False
             lte.isactive = False
             wifi.traffic_transmission = True
+
+switching()
