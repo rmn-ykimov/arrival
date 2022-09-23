7cd0b418706f5940aaaa4216d871e956e49a4f56
Roman Yakimov <85025748+rmn-ykimov@users.noreply.github.com>
2022-09-23 13:33:17 +0400

    test interfaces state refactoring

--- tests/test_interfaces_state.py
+++ tests/test_interfaces_state.py
@@ -2,15 +2,15 @@
 Module providing checking of network interface's state
 """
 import allure
-import pytest
 
-from switcher import wifi, lte
+from helper import assert_value
+from switcher import wifi, lte, NetworkInterface
 
 
 @allure.title('Interfaces active')
 def test_interfaces_active():
     """
-    Checking that at least one interface is active
+    Checking that at least one interface is active.
     """
     if wifi.is_active is False:
         assert lte.is_active is True
@@ -22,9 +22,11 @@ def test_interfaces_active():
 
 
 @allure.title('Interfaces inactive')
-def test_both_interfaces_inactive():
+def test_both_interfaces_inactive(get_wifi_object: dict):
     """
-    Checking that excludes the situation when both interfaces are inactive
+    Checking that excludes the situation when both interfaces are inactive.
     """
-    with pytest.raises(AssertionError):
-        assert wifi.is_active is False and lte.is_active is False
+    wifi: NetworkInterface = get_wifi_object['wifi']
+    assert_value(actual_value=wifi.is_active,
+                 expected_value=True) and assert_value(
+        actual_value=lte.is_active, expected_value=False)
