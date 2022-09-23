21cf9990bce528dc00c029ea6bd3efa02fe92adb
Roman <85025748+rmn-ykimov@users.noreply.github.com>
2022-09-23 11:20:15 +0400

    test credentials refactoring

---
+++ helper/__init__.py
@@ -0,0 +1,17 @@
+import allure
+
+
+@allure.step('Value checking')
+def assert_value(actual_value: [str, bool], expected_value: [str, bool]):
+    """
+    Network interfaces config values checking
+
+    :param actual_value: Actual config value
+    :param expected_value: Expected config value
+    """
+    if isinstance(actual_value, bool):
+        assert actual_value is expected_value, (f'Wrong {actual_value=}, '
+                                                f'expected value: {expected_value}')
+    else:
+        assert actual_value == expected_value, (f'Correct {actual_value=}, '
+                                                f'expected value: {expected_value}')
--- tests/test_credentials.py
+++ tests/test_credentials.py
@@ -3,52 +3,49 @@ Module providing checking if network interface's credentials are valid
 """
 import allure
 import pytest
+
 from config.config import USERNAME, PASSWORD
+from helper import assert_value
 from switcher import NetworkInterface
-from switcher import lte
-from switcher import wifi
 
 
 @allure.title('Wi-Fi credentials')
-def test_wifi_credentials():
+def test_wifi_credentials(get_wifi_object: dict):
     """
-    Checking that username and password for wi-fi are valid.
+    Checking that username and password for Wi-Fi are valid.
     """
-    assert wifi.username == USERNAME
-    assert wifi.password == PASSWORD
+    wifi: NetworkInterface = get_wifi_object['wifi']
+    assert_value(actual_value=wifi.username, expected_value=USERNAME)
+    assert_value(wifi.password, PASSWORD)
 
 
 @allure.title('LTE credentials')
-def test_lte_credentials():
+def test_lte_credentials(get_lte_object: dict):
     """
-    Checking that lte password and username are valid.
+    Checking that username and password for LTE are valid.
     """
-    assert lte.username is None
-    assert lte.password is None
+    lte: NetworkInterface = get_lte_object['lte']
+    assert_value(actual_value=lte.username, expected_value=None)
+    assert_value(lte.password, None)
 
 
 @allure.title('Wi-Fi credentials (negative)')
-@pytest.mark.xfail()
-def test_wifi_credentials_negative():
+@pytest.mark.xfail
+def test_wifi_credentials(get_wifi_object: dict):
     """
-    Checking that Wi-Fi credentials is not incorrect
+    Checking that Wi-Fi credentials is not incorrect.
     """
-    assert wifi.username != USERNAME
-    assert wifi.password != PASSWORD
+    wifi: NetworkInterface = get_wifi_object['wifi']
+    assert_value(actual_value=wifi.password, expected_value=USERNAME)
+    assert_value(wifi.username, PASSWORD)
 
 
 @allure.title('LTE credentials (negative)')
 @pytest.mark.xfail()
-def test_lte_credentials_negative():
+def test_lte_credentials(get_lte_object: dict):
     """
-    Checking that LTE credentials is not incorrect
+    Checking that LTE credentials is not incorrect.
     """
-    assert lte.username is not None
-    assert lte.password is not None
-
-@allure.title('My test')
-def test_wifi_credentials(get_class: dict):
-    """Checking that username and password for wi-fi are valid."""
-    wifi: NetworkInterface = get_class['wifi']
-    assert_value(actual_value=wifi.username, expected_value=USERNAME)
-    assert_value(wifi.password, PASSWORD)
+    lte: NetworkInterface = get_lte_object['lte']
+    assert_value(actual_value=lte.username, expected_value=USERNAME)
+    assert_value(lte.password, PASSWORD)
